37bdf3443dfbe2952e610c892379b15337ac7fed
oops <oops@777126ae-65cd-11e0-aec0-faffbd8595f4>
2014-02-14 06:43:26 +0000

    tagging 1.0.4

---
+++ tags/1.0.4/acconfig.h
@@ -0,0 +1,12 @@
+/* $Id$ */
+#ifndef CHARDEC_CONFIG_H
+#define CHARDEC_CONFIG_H
+
+
+/* config.h:  a general config file */
+
+@TOP@
+
+@BOTTOM@
+
+#endif /*  CHARDEC_CONFIG_H */
---
+++ tags/1.0.4/chardet-config.h
@@ -0,0 +1,101 @@
+/* chardet-config.h.  Generated from chardet-config.h.in by configure.  */
+/* chardet-config.h.in.  Generated from configure.ac by autoheader.  */
+/* $Id$ */
+#ifndef CHARDEC_CONFIG_H
+#define CHARDEC_CONFIG_H
+
+
+/* config.h:  a general config file */
+
+
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1, depending whether the compiler DLL_EXPORT declarations. */
+/* #undef HAVE_DLL_EXPORT */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if your system has a GNU libc compatible `malloc' function, and
+   to 0 otherwise. */
+#define HAVE_MALLOC 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if `stat' has the bug that it succeeds when given the
+   zero-length file name argument. */
+/* #undef HAVE_STAT_EMPTY_STRING_BUG */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1, depending whether the compiler supports simple visibility
+   declarations. */
+#define HAVE_VISIBILITY 1
+
+/* Define to 1 if `lstat' dereferences a symlink specified with a trailing
+   slash. */
+#define LSTAT_FOLLOWS_SLASHED_SYMLINK 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "libchardet"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://oops.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "libchardet"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libchardet 1.0.4"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "libchardet"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0.4"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
+#define TIME_WITH_SYS_TIME 1
+
+/* Version number of package */
+#define VERSION "1.0.4"
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to rpl_malloc if the replacement function should be used. */
+/* #undef malloc */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
+
+#endif /*  CHARDEC_CONFIG_H */
