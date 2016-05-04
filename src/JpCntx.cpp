faa209bfd48b8e033a9c1f5738d5747d0ef69799
JoungKyun Kim <joungkyun@gmail.com>
2016-05-05 01:00:20 +0900

    add vim environment variables

--- src/JpCntx.cpp
+++ src/JpCntx.cpp
@@ -1,4 +1,6 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * vim600: et sw=2 ts=2 fdm=marker
+ */
 /* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
